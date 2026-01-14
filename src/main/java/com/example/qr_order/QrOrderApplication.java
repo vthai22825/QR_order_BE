6187bf6a2290a6f312f279b1556f11f9da899e3f
vthai <vietthai22825@outlook.com>
2026-01-14 21:02:21 +0700

    First commit

---
+++ src/main/java/com/example/qr_order/QrOrderApplication.java
@@ -0,0 +1,13 @@
+package com.example.qr_order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QrOrderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QrOrderApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/qr_order/QrOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.qr_order;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QrOrderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
