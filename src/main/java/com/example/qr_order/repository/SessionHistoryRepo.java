0cac6fcbcafd49472f29f92ad6540a9fa53b0a9a
vthai <vietthai22825@outlook.com>
2026-02-20 21:11:38 +0700

    delete session history

--- src/main/java/com/example/qr_order/repository/SessionHistoryRepo.java
+++
@@ -1,9 +0,0 @@
-package com.example.qr_order.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.example.qr_order.entity.SessionHistory;
-import java.util.List;
-
-public interface SessionHistoryRepo extends JpaRepository<SessionHistory, Long> {
-    List<SessionHistory> findByTableId(Long tableId);
-}
